f1af90304a7481974a2bc496e62c1b80aef06a98
PureumJeon <jpurm0724@gmail.com>
2020-09-10 17:26:47 +0900

    ~4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
-  minus = () => {
-    this.setState((current) => ({ count: current.count + -1 }));
-  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
 
   render() {
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        <h1>the number is {this.state.count}.</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading
+          ? "Loading.."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
       </div>
     );
   }
