6213c4a26d881daa34bb93f66c5ff6e8f368cce2
PureumJeon <jpurm0724@gmail.com>
2020-09-18 17:33:57 +0900

    Updates

---
+++ precache-manifest.4988e5bbd3c53eb70a8a13fc5a2115e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9c0445201846150eb7a0ed52c4ada4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4d48afd5a3a893ea0731",
+    "url": "/movie_app/static/css/main.6a1d0458.chunk.css"
+  },
+  {
+    "revision": "48d04c1484dc82f93f2a",
+    "url": "/movie_app/static/js/2.77275e5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.77275e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d48afd5a3a893ea0731",
+    "url": "/movie_app/static/js/main.85fd178d.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
